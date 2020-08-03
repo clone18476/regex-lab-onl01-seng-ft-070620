5e2b66977fa112ff0258b2aa4b98b8bdaed2b972
Cameron Cousins <cameroncousins18@gmail.com>
2020-08-03 14:47:15 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -20,6 +20,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
- phone.scan(/^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/)
-
+  phone.match?/\(?\d{3}\)?(\s|-)?\d{3}(\s|-)?\d{4}/
 end
