ea366697c2c3e29f01ba0572434638ef5eb4b662
Cameron Cousins <cameroncousins18@gmail.com>
2020-08-03 14:13:16 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -20,7 +20,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
- # phone.scan(/(\d+)-(\d+)-(\d+)/) 
+  phone.scan? /(\d+)-(\d+)-(\d+)/  
  	
-phone.scan? ^[2-9]\d{2}-\d{3}-\d{4}$)
+# phone.scan? ^[2-9]\d{2}-\d{3}-\d{4}$
 end
